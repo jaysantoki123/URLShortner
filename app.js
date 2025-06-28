0b26ae5a19e6b4ef66956084899d9513ad042027
jaysantoki123 <158308651+jaysantoki123@users.noreply.github.com>
2025-06-28 11:33:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ const server = createServer(async (req, res) => {
 
 })
 
-const PORt = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
